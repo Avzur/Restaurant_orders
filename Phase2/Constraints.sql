62290fb84a580f7d8cea471e0331fe3bcf9f7022
Avzur <azur@g.jct.ac.il>
2026-04-28 14:34:40 +0300

    add update files

--- Phase2/Constraints.sql
+++ Phase2/Constraints.sql
@@ -2,23 +2,16 @@
 -- CONSTRAINTS
 -- =========================================
 
--- 1. מספר אנשים מקסימלי להזמנה
 ALTER TABLE RESERVATION
 ADD CONSTRAINT max_people_check
 CHECK (NumberOfPeople <= 20);
 
 
--- =========================================
-
--- 2. מספר טלפון חייב להיות 9 ספרות לפחות
 ALTER TABLE SERVICEPROVIDER
 ADD CONSTRAINT phone_length_check
 CHECK (Phone >= 100000000);
 
 
--- =========================================
-
--- 3. תאריך סיום קופון לא יכול להיות NULL
 ALTER TABLE COUPON
 ALTER COLUMN EndDate SET NOT NULL;
 
